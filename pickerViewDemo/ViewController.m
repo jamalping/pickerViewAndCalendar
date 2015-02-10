272a7752df884b2369f50429a927893555008af8
jamalping <silvercreek@Ryans-MBA.local>
2015-02-10 17:15:50 +0800

    Initial Commit

---
+++ pickerViewDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  pickerViewDemo
+//
+//  Created by jamalping on 15-2-10.
+//  Copyright (c) 2015年 jamalping. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
